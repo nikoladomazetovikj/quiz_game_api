a4d02742fa6686e16fceb3c4dd7e14f91ae9f375
nikolla.d <nikolad150@gmail.com>
2022-12-17 00:51:42 +0100

    initial commit

---
+++ src/test/java/com/nikola/quiz_game/QuizGameApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nikola.quiz_game;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class QuizGameApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
