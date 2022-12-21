aa0268164adf097a1bf42f393c031bd5938ba657
nikolla.d <nikolad150@gmail.com>
2022-12-21 22:07:50 +0100

    feat: methods in question repository

--- src/main/java/com/nikola/quiz_game/repository/QuestionRepository.java
+++ src/main/java/com/nikola/quiz_game/repository/QuestionRepository.java
@@ -4,7 +4,11 @@ import com.nikola.quiz_game.pojo.Question;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 @Repository
 public interface QuestionRepository extends CrudRepository <Question, Integer> {
 
+    List<Question> findAl();
+    Question findQuestionById(Integer id);
 }
